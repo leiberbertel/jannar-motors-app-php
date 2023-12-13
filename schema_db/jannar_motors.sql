1e45e9becea77250296161ddfb6b70234c24fc22
leiber bertel <leiberbertel777@gmail.com>
2023-12-12 21:50:38 -0500

    refactor: update models

--- schema_db/jannar_motors.sql
+++ schema_db/jannar_motors.sql
@@ -11,3 +11,17 @@ CREATE TABLE vehiculos (
     descripcion VARCHAR(200) NOT NULL,
     ruta_imagen VARCHAR(255)
 );
+
+CREATE TABLE rol (
+    idrol INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    rol VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE usuario (
+    idusuario int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(40) NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    usuario VARCHAR(40) NOT NULL,
+    rol INT(11) NOT NULL,
+    contrasena VARCHAR(50) NOT NULL
+);
